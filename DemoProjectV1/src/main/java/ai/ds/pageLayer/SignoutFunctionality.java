ad3d130ece0d7934a6326feb8d8a3d44cb08cb0e
DarshanTavandkar <119282216+DarshanTavandkar@users.noreply.github.com>
2022-12-14 14:20:17 +0530

    change

---
+++ DemoProjectV1/src/main/java/ai/ds/pageLayer/SignoutFunctionality.java
@@ -0,0 +1,14 @@
+package ai.ds.pageLayer;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignoutFunctionality {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I Love India");
+	}
+
+}
